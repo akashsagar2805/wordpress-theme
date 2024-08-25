a7de3e9c017d133e74ad375e99d28f72a82eae78
akashsagar2805 <akashsagar5869@gmail.com>
2024-08-25 12:04:30 +0530

    main post loop

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 
   // Theme Support
   function wpb_theme_setup(){
+    add_theme_support( 'post-thumbnails' );
     // Nav Menus
     register_nav_menus(array(
       'primary' => __('Primary Menu')
@@ -11,3 +12,9 @@
   }
 
   add_action('after_setup_theme','wpb_theme_setup');
+
+  function set_excerpt_length(){
+    return 45;
+  }
+
+  add_filter('excerpt_length', 'set_excerpt_length');
