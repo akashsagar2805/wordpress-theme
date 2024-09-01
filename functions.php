0f32deba6d32f6f2a132731c77681c6d22da97b4
akashsagar2805 <akashsagar5869@gmail.com>
2024-09-01 18:22:35 +0530

    added post formats

--- functions.php
+++ functions.php
@@ -6,10 +6,15 @@ require_once('wp_bootstrap_navwalker.php');
 function wpb_theme_setup()
 {
   add_theme_support('post-thumbnails');
+
   // Nav Menus
   register_nav_menus(array(
     'primary' => __('Primary Menu')
   ));
+
+  //Post Formats
+  add_theme_support('post-formats', array('aside', 'gallery'));
+
 }
 
 add_action('after_setup_theme', 'wpb_theme_setup');
